98bcf6a082a0a5384a88ec6337709048c790e07f
johnny dev <coding.with.johnny.dev@gmail.com>
2024-06-18 23:22:42 -0600

    feat: add database integration with postgresql and typeorm

--- README.md
+++ README.md
@@ -4,7 +4,12 @@
 
 # teslo API
 
-1. Levantar la base de datos
+1. Clonar proyecto
+2. Instalar dependencias ```yarn install```
+3. Clonar el archivo ```.env.template``` y renombrarlo a ```.env```
+4. Cambiar las variables de entorno
+5. Levantar la base de datos
 ```bash
 docker-compose up -d
 ```
+6. Ejecutar el proyecto ```yarn start:dev```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
